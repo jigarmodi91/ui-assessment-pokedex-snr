e91536df5024d6917233bb443441e09ea12596c6
Jigar Modi <jigar.modi@druva.com>
2025-09-20 10:55:16 +0530

    adds search capability to filter pokemon on the bases of input text

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import { useGetPokemons } from '../../hooks/useGetPokemons';
 import { PokemonCard } from './PokemonCard';
@@ -6,12 +6,33 @@ import { PokemonCard } from './PokemonCard';
 export const PokemonList = () => {
   const classes = useStyles();
   const { pokemons, loading } = useGetPokemons();
+  const [search, setSearch] = useState('');
+  const filteredPokemons = useMemo(() => {
+    return search
+      ? pokemons.filter((pkmn) =>
+          pkmn.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : pokemons;
+  }, [pokemons, search]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div className={classes.root}>
       {loading && <div>Loading...</div>}
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          className={classes.search}
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
       <div className={classes.pokemonList}>
-        {pokemons.map((pkmn) => (
+        {filteredPokemons.map((pkmn) => (
           <PokemonCard key={pkmn.id} pokemon={pkmn} />
         ))}
       </div>
@@ -31,6 +52,16 @@ const useStyles = createUseStyles(
       display: 'flex',
       flexWrap: 'wrap',
       justifyContent: 'center',
+      paddingTop: '50px',
+    },
+    search: {
+      width: '50%',
+      padding: '10px',
+      marginBottom: '10px',
+      borderRadius: '5px',
+      border: '1px solid #ccc',
+      fontSize: '16px',
+      color: '#333333',
     },
   },
   { name: 'PokemonList' }
